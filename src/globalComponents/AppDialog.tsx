9bca7c4dc79e64f9dcd40d8dd9333736c6d4ef3d
tamanjic <tamanji.che@metropolia.fi>
2022-01-26 14:30:54 +0200

    Added <AppDialog /> utility

---
+++ src/globalComponents/AppDialog.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
+export interface AppDialogProps {
+  id: string;
+  children?: React.ReactNode;
+  open: boolean;
+  title: string;
+  onClose: () => void;
+}
+
+export type AppDialogTitleProps = Omit<AppDialogProps, 'open' | 'title'>;
+
+const AppDialogTitle = ({
+  children,
+  onClose,
+  ...other
+}: AppDialogTitleProps) => {
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+};
+
+const AppDialog = ({ children, onClose, open, title }: AppDialogProps) => {
+  return (
+    <div>
+      <Dialog onClose={onClose} aria-labelledby="dialog-title" open={open}>
+        <AppDialogTitle id="dialog-title" onClose={onClose}>
+          {title}
+        </AppDialogTitle>
+        <DialogContent dividers>{children}</DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+export default AppDialog;
